744b06525ccf68db7bfc2e0c76ef6e07a075df76
Antonio Joaquín <antoniojoaquinluque04@gmail.com>
2020-10-18 10:13:20 +0200

    - Created Bloc files: states, events, and bloc manager, to get pokemon list -

---
+++ lib/src/bloc/pokemons/PokemonsBloc.dart
@@ -0,0 +1,28 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_pokedex/src/bloc/pokemons/PokemonsEvent.dart';
+import 'package:flutter_pokedex/src/bloc/pokemons/PokemonsState.dart';
+import 'package:flutter_pokedex/src/repository/PokemonRepository.dart';
+
+class PokemonsBloc extends Bloc<PokemonsEvent, PokemonsState> {
+  PokemonRepository pokemonRepository;
+
+  PokemonsBloc({@required this.pokemonRepository})
+      : assert(pokemonRepository != null);
+
+  @override
+  PokemonsState get initialState => WithoutPokemonsState();
+
+  @override
+  Stream<PokemonsState> mapEventToState(PokemonsEvent event) async* {
+    if (event is AddMorePokemons) {
+      try {
+        final pokemons = await this.pokemonRepository.fetchPokemons();
+
+        yield (WithPokemonsState(pokemons: pokemons, amount: pokemons.length));
+      } catch (error) {
+        print(error);
+      }
+    }
+  }
+}
